6f8e7aca8579d2624bb40581dcc90739d6bf95f2
eversonfx <eversonfelix_mb@hotmail.com>
2021-07-31 18:20:05 -0300

    Testando a primeira classe de domínio - Categoria

--- src/main/java/com/everson/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/everson/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,26 @@
 package com.everson.cursomc.resources;
 
+import com.everson.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String listar() {
-        return "REST está funcionando!";
+    public List<Categoria> listar() {
+        Categoria cat1 = new Categoria(1, "Informatica");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categoriaList = new ArrayList<>();
+        categoriaList.add(cat1);
+        categoriaList.add(cat2);
+
+        return categoriaList;
     }
 }
