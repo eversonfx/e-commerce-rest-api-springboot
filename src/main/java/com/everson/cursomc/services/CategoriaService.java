32c09b0bb0c5e7f9308dc8dd57346f6438d14d6d
eversonfx <eversonfelix_mb@hotmail.com>
2021-08-04 20:22:42 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/everson/cursomc/services/CategoriaService.java
+++ src/main/java/com/everson/cursomc/services/CategoriaService.java
@@ -1,9 +1,11 @@
 package com.everson.cursomc.services;
 
 import com.everson.cursomc.domain.Categoria;
+import com.everson.cursomc.services.exceptions.DataIntegrityException;
 import com.everson.cursomc.services.exceptions.ObjectNotFoundException;
 import com.everson.cursomc.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -33,5 +35,14 @@ public class CategoriaService {
     public Categoria update(Categoria obj) {
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch(DataIntegrityViolationException e) {
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
     }
 }
