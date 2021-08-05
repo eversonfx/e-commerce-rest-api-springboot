ec284b7dacb43410f638e17e367447f03649e821
eversonfx <eversonfelix_mb@hotmail.com>
2021-08-05 18:27:53 -0300

    Validação customizada: email nao repetido na insercao de cliente

--- src/main/java/com/everson/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/everson/cursomc/repositories/ClienteRepository.java
@@ -3,6 +3,7 @@ package com.everson.cursomc.repositories;
 import com.everson.cursomc.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 /*
     JpaRepository é um tipo especial do Spring capaz de acessar os dados com
@@ -10,5 +11,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 
 }
