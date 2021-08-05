593f18b6d172fc19ac4965a64b98c1d91f668266
eversonfx <eversonfelix_mb@hotmail.com>
2021-08-05 16:28:25 -0300

    Ajustando e testando a delecao de Cliente

--- src/main/java/com/everson/cursomc/domain/Cliente.java
+++ src/main/java/com/everson/cursomc/domain/Cliente.java
@@ -17,7 +17,7 @@ public class Cliente {
     private Integer tipo;
 
 
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
     private List<Endereco> enderecos = new ArrayList<>();
 
     /*
